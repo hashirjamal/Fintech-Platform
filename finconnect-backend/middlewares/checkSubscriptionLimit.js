ee411facf29705c7398b3d912b5a1ffebc60dc87
DEEPAK CHOWDARY KANDULA <u2835127@uel.ac.uk>
2025-10-29 22:41:42 +0000

    checkSubscriptionLimit - Business Logic Limiter: Enforces subscription-based transaction limits, Based on user's paid subscription plan

---
+++ finconnect-backend/middlewares/checkSubscriptionLimit.js
@@ -0,0 +1,63 @@
+const Limit = require("../models/Limit");
+const Subscription = require("../models/Subscription");
+const User = require("../models/User");
+const asyncErrorHandler = require("../utils/asyncErrorHandler");
+
+const checkSubscriptionLimit = asyncErrorHandler(async (req, res, next) => {
+    const { sender, amount } = req.body;
+
+    if (!sender || typeof amount !== "number") {
+        return next({
+            status: 400,
+            message: "Missing or invalid accountNumber or amount",
+        });
+    }
+
+    const user = await User.findOne({
+        where: { accountNumber: sender },
+        include: {
+            model: Subscription,
+            as: "Subscription",
+            attributes: ["transactionLimit", "price","transactionPerDay"],
+        },
+    });
+
+    if (!user || !user.subscriptionId) {
+        return next({
+            status: 404,
+            message: "User or their subscription not found",
+        });
+    }
+
+    const limit = await Limit.findOne({
+        where: { userId: sender },
+    });
+    console.log(limit.dataValues)
+    if (!limit) {
+        return next({
+            status: 403,
+            message: "Limit record not found for user",
+        });
+    }
+console.log(user.Subscription,"DVVS")
+    const maxTransactions = user.Subscription.transactionPerDay;
+    const maxAmount = user.Subscription.transactionLimit;
+    const totalTransactionsAfterThis = limit.noOfTransactions + 1;
+    const totalAmountAfterThis = limit.transactionAmount + amount;
+
+   
+
+    if (
+        totalTransactionsAfterThis > maxTransactions ||
+        totalAmountAfterThis > maxAmount
+    ) {
+        return next({
+            status: 403,
+            message: "Transaction limit exceeded for your subscription plan",
+        });
+    }
+
+    next();
+});
+
+module.exports = checkSubscriptionLimit;
